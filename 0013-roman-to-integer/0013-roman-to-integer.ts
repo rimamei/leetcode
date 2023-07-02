78bbfd4592c03bc82fa4915f4180f21d2b3584bc
Rima Mei Handayani <54443034+rimamei@users.noreply.github.com>
2023-07-02 23:48:46 +0700

    Time: 141 ms (41.52%), Space: 49.4 MB (32.58%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.ts
@@ -0,0 +1,16 @@
+const roman = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000
+}
+
+function romanToInt(s: string): number {
+    const arr = s.split('').map(r => roman[r])
+    const result = arr.reduce((acc, x, i) => x < arr[i + 1] ? acc - x : acc + x, 0)
+
+    return result
+};
